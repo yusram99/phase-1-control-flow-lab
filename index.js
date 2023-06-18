3df8b04e8532ba7796899daeaa7f9bc7eece8e83
Your Name <youremail@example.com>
2023-06-18 16:48:31 +0300

    commit message

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function: ternaryCheckCity
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function: switchOnCharmFromTip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
+
+
+
+
+
